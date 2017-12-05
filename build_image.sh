0dc79b7694785b09854740985046363a99eefe3c
unclejack <unclejack@users.noreply.github.com>
2017-12-05 22:04:31 +0200

    Dockerfile,build_image.sh,ovsInit.sh: import
    Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com>

---
+++ build_image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+REPOSITORY="contiv/ovs"
+IMAGE=$REPOSITORY:${NETPLUGIN_CONTAINER_TAG:-latest}
+
+function get_image_id() {
+	docker inspect --format '{{.ID}}' $IMAGE || :
+}
+
+old_image=$(get_image_id)
+
+docker build -t $IMAGE -t $REPOSITORY:latest .
+
+new_image=$(get_image_id)
+
+if [ -n "$old_image" ] && [ "$old_image" != "$new_image" ]; then
+	echo Removing old image $old_image
+	docker rmi -f $old_image >/dev/null 2>&1 || true
+fi
